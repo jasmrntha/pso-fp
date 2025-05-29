ec33450c5b8f1543238d7c6e26a437f83a7aedad
devbetau@gmail.com <root@srv830703.hstgr.cloud>
2025-05-29 06:07:27 +0000

    chore : change config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from 'axios';
 import { UninterceptedApiError } from '@/types/api';
 
 const api = axios.create({
-  baseURL: process.env.PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
